3cb77864af9238881221cede5fa8b06457ec087c
lenieck <56296543+lenieck@users.noreply.github.com>
2021-04-19 23:08:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,24 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
+
+@app.post("/method", status_code = 201)
+def POST():
+    return {"method": "POST"}
+
+@app.get("/method")
+def GET():
+    return {"method": "GET"}
+
+@app.delete("/method")
+def DELETE():
+    return {"method": "DELETE"}
+
+@app.put("/method")
+def PUT():
+    return {"method": "PUT"}
+
+@app.options("/method")
+def OPTIONS():
+    return {"method": "OPTIONS"}
+
