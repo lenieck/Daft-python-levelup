09867e4874d4ea5919831b659da0c00407988fe5
lenieck <56296543+lenieck@users.noreply.github.com>
2021-04-20 17:25:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,11 +25,3 @@ def PUT():
 @app.options("/method")
 def OPTIONS():
     return {"method": "OPTIONS"}
-
-@app.get('/auth', status_code=204)
-def auth(password: Optional[str] = None, password_hash: Optional[str] = None):
-    if not password or not password_hash:
-        raise HTTPException(status_code=401)
-    password = password.encode()
-    if sha512(password).hexdigest() != password_hash:
-        raise HTTPException(status_code=401)
