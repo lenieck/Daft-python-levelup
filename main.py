4d1146a1241f0b7fb067522fc23b5823d27d180d
lenieck <56296543+lenieck@users.noreply.github.com>
2021-04-20 22:36:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -54,6 +54,11 @@ def register(patient: Patient):
     name_length = len([i for i in patient.name if i.isalpha()])
     surname_length = len([i for i in patient.surname if i.isalpha()])
     vaccination_date = today + timedelta(days=(name_length + surname_length))
+    patients[0].append(int(app.counter))
+    patients[1].append(patient.name)
+    patients[2].append(patient.surname)
+    patients[3].append(str(register_date))
+    patients[4].append(str(vaccination_date))
     return {"id": app.counter, "name:":  patient.name, "surname:": patient.surname, "register_date": register_date, "vaccination_date": vaccination_date}
 
 @app.get("/patient/{id}",status_code=200)
