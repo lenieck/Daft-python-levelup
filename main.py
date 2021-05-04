d5e0e0559dd7d3791fe5353bde03f53a329bde69
lenieck <56296543+lenieck@users.noreply.github.com>
2021-05-04 23:45:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -96,12 +96,7 @@ app.login_tokens = []
 def login_session(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
     if credentials.username == "4dm1n" or credentials.password == "NotSoSecurePa$$":
         session_token = sha256(f"4dm1nNotSoSecurePa$${app.secret_key}".encode()).hexdigest()
-        if len(app.access_tokens) < 3:
-            app.access_tokens.append(session_token)
-        else:
-            app.access_tokens[0] = app.access_tokens[1]
-            app.access_tokens[1] = app.access_tokens[2]
-            app.access_tokens[2] = session_token
+        app.access_tokens.append(session_token)
         response.set_cookie(key="session_token", value=session_token)
     else:
         response.status_code = 401
@@ -111,13 +106,7 @@ def login_session(response: Response, credentials: HTTPBasicCredentials = Depend
 def login_session(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
     if credentials.username == "4dm1n" or credentials.password == "NotSoSecurePa$$":
         session_token = sha256(f"4dm1nNotSoSecurePa$${app.secret_key}".encode()).hexdigest()
-        if len(app.login_tokens) < 3:
-            app.login_tokens.append(session_token)
-        else:
-            app.login_tokens[0] = app.login_tokens[1]
-            app.login_tokens[1] = app.login_tokens[2]
-            app.login_tokens[2] = session_token
-        response.set_cookie(key="session_token", value=session_token)
+        app.login_tokens.append(session_token)
     else:
         response.status_code = 401
         raise HTTPException(status_code=401)
